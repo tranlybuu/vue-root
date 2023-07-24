c14504577025a9a3c70d460088c42890f5b43696
tranlybuu <lybuu9117@gmail.com>
2023-07-24 17:32:56 +0700

    upload

--- src/pages/example/index.vue
+++ src/pages/example/index.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="container py-4 grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-2">
+  <div class="container py-4 grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-2">
     <div class="flex flex-col gap-4">
       <h1 class="font-extrabold text-2xl">Vue Example Page</h1>
       <ul>
-        <li>Bootstrap</li>
-        <li>TailwindCss</li>
-        <li>VueRouter</li>
-        <li>Axios</li>
-        <li>VueX</li>
-        <li>SweetAlert2</li>
-        <li>VueViewer</li>
-        <li>VuePageTransition</li>
-        <li>VueMultiselect</li>
-        <li>Vue3-flicking</li>
+        <li><a class="text-lg" target="_blank" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">Bootstrap 5</a></li>
+        <li><a class="text-lg" target="_blank" href="https://tailwindcss.com/">TailwindCss</a></li>
+        <li><a class="text-lg" target="_blank" href="https://router.vuejs.org/">VueRouter</a></li>
+        <li><a class="text-lg" target="_blank" href="https://www.npmjs.com/package/axios">Axios</a></li>
+        <li><a class="text-lg" target="_blank" href="https://vuex.vuejs.org/">VueX</a></li>
+        <li><a class="text-lg" target="_blank" href="https://sweetalert2.github.io/">SweetAlert2</a></li>
+        <li><a class="text-lg" target="_blank" href="https://github.com/mirari/v-viewer/tree/v3">VueViewer</a></li>
+        <li><a class="text-lg" target="_blank" href="https://www.npmjs.com/package/vue-page-transition">VuePageTransition</a></li>
+        <li><a class="text-lg" target="_blank" href="https://vue-multiselect.js.org/">VueMultiselect</a></li>
+        <li><a class="text-lg" target="_blank" href="https://naver.github.io/egjs-flicking/">Vue3-flicking</a></li>
       </ul>
     </div>
-    <div class="flex flex-col gap-4">
+    
+    <div class="flex flex-col gap-6 md:col-span-2">
       <h1 class="text-blue-500 text-xl font-semibold">Example</h1>
 
       <div>
-        <p class="text-lg">Bootstrap 5</p>
+        <p class="text-lg">Bootstrap 5 - TailwindCss</p>
         
       </div>
   
@@ -41,32 +42,37 @@
       
       <div>
         <Flicking ref="flicking" 
-          :options="{ circular: true, moveType: 'freeScroll' }"
+          :options="flickingOptions"
           :viewportTag="'div'"
           :cameraTag="'div'"
           :cameraClass="''"
           :plugins="plugins"
           @move-end="onMoveEnd">
-          <div class="relative panel w-52 h-40 bg-red-700">
+          <div class="relative panel w-full h-64 bg-red-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">1</p>
           </div>
-          <div class="relative panel w-52 h-40 bg-blue-700">
+          <div class="relative panel w-full h-64 bg-blue-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">2</p>
           </div>
-          <div class="relative panel w-52 h-40 bg-green-700">
+          <div class="relative panel w-full h-64 bg-green-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">3</p>
           </div>
-          <div class="relative panel w-52 h-40 bg-slate-700">
+          <div class="relative panel w-full h-64 bg-slate-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">4</p>
           </div>
-          <div class="relative panel w-52 h-40 bg-yellow-700">
+          <div class="relative panel w-full h-64 bg-yellow-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">5</p>
           </div>
-          <div class="relative panel w-52 h-40 bg-amber-700">
+          <div class="relative panel w-full h-64 bg-amber-700">
             <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-3xl text-white">6</p>
           </div>
+          <template #viewport>
+            <!-- <span class="flicking-arrow-prev is-thin is-circle"></span>
+            <span class="flicking-arrow-next is-thin is-circle"></span> -->
+            <div class="flicking-pagination"></div>
+          </template>
         </Flicking>
-        <div class="flex gap-2">
+        <div class="flex gap-2 justify-center">
           <button class="btn btn-outline-dark" @click="clickToBackSlide">Back slide</button>
           <button class="btn btn-outline-dark" @click="clickToNextSlide">Next slide</button>
         </div>
@@ -78,10 +84,18 @@
 <script>
 import { mapActions } from 'vuex';
 import Flicking from "@egjs/vue3-flicking";
-import { Fade, AutoPlay } from "@egjs/flicking-plugins";
+import { Fade, AutoPlay, Pagination, Perspective } from "@egjs/flicking-plugins";
 import VueMultiselect from 'vue-multiselect'
 
-const plugins = [new Fade(), new AutoPlay({ duration: 1000, direction: "NEXT", stopOnHover: false })]
+// import "@egjs/flicking-plugins/dist/arrow.css";
+import "@egjs/flicking-plugins/dist/pagination.css";
+
+const plugins = [
+  new Perspective({ rotate: -1, scale: 2, perspective: 600 }),
+  new Pagination({  type: 'scroll' }), 
+  new Fade(), 
+  new AutoPlay({ duration: 1000, direction: "NEXT", stopOnHover: false })
+]
 
 export default {
   name: "example-page",
@@ -94,6 +108,11 @@ export default {
       selected: null,
       options: ['list', 'of', 'options'],
       isOpen: false,
+      flickingOptions: { 
+        circular: true, 
+        moveType: 'freeScroll',
+        panelsPerView: 3
+      },
       plugins
     }
   },
